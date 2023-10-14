f19c56ad7b891a21401c016815dc5ecc03822508
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-10-14 20:38:42 +0300

    updating packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,46 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ContactsProvider from "./features/contacts/ContactsProvider";
+import Contacts from "./pages/Contacts";
+import { Toaster } from "react-hot-toast";
 
-import styled from "styled-components";
-import ContactsList from "./features/contacts/ContactsList";
-import ContactForm from "./features/contacts/ContactForm";
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 function App() {
   return (
-    <Main>
-      <ContainerWrapper>
-        {/* <ContactsList /> */}
-        <ContactForm />
-      </ContainerWrapper>
-    </Main>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ContactsProvider>
+        <Contacts />
+      </ContactsProvider>
+      <Toaster
+        position="bottom-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--sky-500)",
+            color: "var(--white-200)",
+          },
+        }}
+      />
+    </QueryClientProvider>
   );
 }
 
-const Main = styled.main`
-  width: 1164px;
-  height: 667px;
-  border-radius: 25px;
-  border: 1px solid #fff;
-  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  height: 100%;
-  padding: 32px;
-  position: relative;
-`;
-
-const ContainerWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 70px;
-
-  height: 100%;
-  width: 100%;
-`;
 export default App;
